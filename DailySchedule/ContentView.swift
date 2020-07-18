567d67d9663d4c2e9595149a50fbb76d9f4d0711
Sabrina Bea <me@sabrinabea.com>
2020-07-18 19:31:53 -0400

    Initial Commit

---
+++ DailySchedule/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  DailySchedule
+//
+//  Created by Sabrina Bea on 7/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DailySchedule/DailyScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyScheduleApp.swift
+//  DailySchedule
+//
+//  Created by Sabrina Bea on 7/18/20.
+//
+
+import SwiftUI
+
+@main
+struct DailyScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
